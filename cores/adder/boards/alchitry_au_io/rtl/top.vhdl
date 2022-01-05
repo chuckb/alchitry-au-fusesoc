287ec66ebdcb6fe3fdef4bc987ad92f4e37436df
Chuck Benedict <itsme@chuckstecktalk.com>
2022-01-05 12:46:07 -0800

    Factor out Makefile

--- cores/adder/boards/alchitry_au_io/rtl/top.vhdl
+++ cores/adder/boards/alchitry_au_io/rtl/top.vhdl
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity au_top is
+entity top is
   port(
     -- Inputs
     io_dip_sw : in std_logic_vector(23 downto 0);
@@ -15,9 +15,9 @@ entity au_top is
     io_ss_led : out std_logic_vector(7 downto 0) := (others => '1');
     io_ss_en : out std_logic_vector(3 downto 0) := (others => '1')
   );
-end au_top;
+end top;
 
-architecture rtl of au_top is
+architecture rtl of top is
   -- Define signals to access adder ports
   signal A : unsigned(3 downto 0);
   signal B : unsigned(3 downto 0);
